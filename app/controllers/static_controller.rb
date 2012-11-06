4713507a6e07717a2c451d28612c07dd74cc8e51
Josh Silverman <joshsilverman@Joshs-MacBook-Air.local>
2012-11-06 00:35:39 -0500

    basic setup

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,11 @@
+class StaticController < ApplicationController
+  
+  def land
+  end
+
+  def activate
+  end
+
+  def engage
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestiveMock::Application.config.secret_token = '195eb6a946ca51888f0bd502c684c57be3df0eee4995ad53d2d36e73186941f7c5a2100db48515830ca98fcfb912b0478933fe78da019807debdfd6224272342'
